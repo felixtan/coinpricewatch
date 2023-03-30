57813d1b9ee655ba24be279380078b8056f2033f
Felix Tan <tan.felix@protonmail.com>
2023-03-29 21:59:03 -0700

    integrate typegraphql, use generated user resolver instead of rest api

--- web-server/src/app.module.ts
+++ web-server/src/app.module.ts
@@ -1,12 +1,29 @@
+import path from 'path';
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaClient } from '@prisma/client';
+import { TypeGraphQLModule } from 'typegraphql-nestjs';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { PrismaService } from './prisma/prisma.service';
+import { UserCrudResolver } from '@generated/type-graphql';
+
+interface Context {
+  prisma: PrismaClient;
+}
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    // TypeGraphQLModule.forRoot<ApolloDriverConfig>({
+    TypeGraphQLModule.forRoot({
+      driver: ApolloDriver,
+      path: '/',
+      emitSchemaFile: path.resolve(__dirname, './generated-schema.graphql'),
+      validate: false,
+      context: (): Context => ({ prisma: new PrismaClient() }),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, UserCrudResolver],
 })
 export class AppModule {}
